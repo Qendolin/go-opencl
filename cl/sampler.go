e80590df0d3c329d5aedf760898dba5794430427
Qendolin <32160662+Qendolin@users.noreply.github.com>
2023-08-23 15:34:49 +0200

    fix sampler.go

--- cl/sampler.go
+++ cl/sampler.go
@@ -2,13 +2,10 @@ package cl
 
 // #include "cl.h"
 import "C"
-import (
-	"github.com/Qendolin/go-opencl/cl"
-)
 
-func (ctx *Context) CreateSampler(normalizedCoords bool, addressingMode cl.SamplerAddressingMode, filterMode cl.SamplerFilterMode) (*MemObject, error) {
+func (ctx *Context) CreateSampler(normalizedCoords bool, addressingMode SamplerAddressingMode, filterMode SamplerFilterMode) (*Sampler, error) {
 	var err C.cl_int
-	clSampler := C.clCreateSampler(ctx.clContext, clBool(normalizedCoords), addressingMode, filterMode, &err)
+	clSampler := C.clCreateSampler(ctx.clContext, clBool(normalizedCoords), C.cl_uint(addressingMode), C.cl_uint(filterMode), &err)
 	if err != C.CL_SUCCESS {
 		return nil, toError(err)
 	}
